57423ad876d8740b82133c2625eae3362580cf81
Ian A <iastesana@gmail.com>
2020-07-25 08:35:55 -0700

    Functionality up to Display function is done.

--- main.js
+++ main.js
@@ -1,12 +1,81 @@
+var questionEl =  document.getElementById("questions");
+var startBtn = document.querySelector('#startButton');
+var timerElem = document.querySelector('#timer');
+var results = document.querySelector('#results');
+var display1= document.querySelector("#display1")
+var display2= document.querySelector("#display2")
+var btn1= document.querySelector("#btn1")
+var btn2= document.querySelector("#btn2")
+var btn3= document.querySelector("#btn3")
+var correctAnsw = 0
+var wrongAnsw = 0
+var userChoice = document.querySelector(".options")
+var timeRemainikng = 90;
+var questionIndex = 0;
+var questions = [
+    {
+        question:'What is constant within our universe?', 
+        choices: ["choice A", "choice B", "choice C"],
+        answer: 0,
+    },
+    
+    {
+        question:'B?', 
+        choices: ["choice A", "choice B", "choice C"],
+        answer: 1,
+    },
+
+    {
+        question:'C?', 
+        choices: ["choice A", "choice B", "choice C"],
+        answer: 2,
+    },
+]
+
+var currentQuestion = 0;
+
+var SECOND = 1000;
+
+questionEl.style["visibility"] = "hidden"
+
+
+results.style["visibility"] = "hidden"
+
+
 function startQuiz(){
-    var questionEl =  document.getElementById("questions")
+    console.log("Start button")
     questionEl.style["visibility"] = "visible"
-    document.getElementById("startSection").style["visibility"] = "hidden"
+    startBtn.style["visibility"] = "hidden"
+    displayQuestion()
 }
 
-document.getElementById("startButton").onclick = startQuiz
+function displayQuestion(){
+    display1.innerText= questions[currentQuestion].question
+    btn1.innerText= questions[currentQuestion].choices[0]
+    btn2.innerText= questions[currentQuestion].choices[1]
+    btn3.innerText= questions[currentQuestion].choices[2]
+}
 
-var questions = [
-    'What is constant within our universe?',
-    'How big is America?'
-]
+function checkUserChoice() {
+    console.log("Checking On Click")
+    var answer = this.getAttribute("data-value")
+    if(answer == questions[currentQuestion].answer){
+        correctAnsw++
+        display2.innerText="Right Answer!"
+    }
+    else{
+        wrongAnsw++
+        display2.innerText="Right Answer!"
+    }
+}
+
+startBtn.onclick = startQuiz
+btn1.onclick = checkUserChoice
+btn2.onclick = checkUserChoice
+btn3.onclick = checkUserChoice
+
+
+
+
+// functionality
+// timers
